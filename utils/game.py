a1d8c4ef1bf02a8f581030da072cd4967121434f
Durieux <durieux.arnaud@hotmail.com>
2021-05-21 10:04:31 +0200

    Made method in the class private and change some string format

--- utils/game.py
+++ utils/game.py
@@ -53,13 +53,13 @@ class Hangman:
             "mathematics",
             "sessions",
         ]
-        self.initialize_game()
+        self.__initialize_game()
 
     @property
     def __error_count(self) -> int:
         return len(self.__wrongly_guessed_letters)
 
-    def initialize_game(self):
+    def __initialize_game(self):
         self.__word_to_find: List[str] = list(
             random.choice(self.__possible_words)
         )
@@ -70,20 +70,20 @@ class Hangman:
         self.__turn_count: int = 1
         self.__wrongly_guessed_letters: List[str] = []
 
-    def play(self) -> None:
+    def __play(self) -> None:
         """This function is in charge of asking the user's input and process it
         and print the result of the turn
         """
         chosen_letter = input("Choose a letter : ").lower()
         if not re.match("^[A-z]$", chosen_letter):
             print("Please enter a valid letter")
-            self.play()
+            self.__play()
         elif (
             chosen_letter in self.__wrongly_guessed_letters
             or chosen_letter in self.__correctly_guessed_letters
         ):
             print("You already tried this letter")
-            self.play()
+            self.__play()
         else:
             if chosen_letter in self.__word_to_find:
                 # adding an empty line for more readability
@@ -106,11 +106,11 @@ class Hangman:
 
             self.__turn_count += 1
 
-    def game_over(self) -> None:
+    def __game_over(self) -> None:
         """Function in charge of printing when the game is over"""
         print("game over...")
 
-    def well_played(self) -> None:
+    def __well_played(self) -> None:
         """function called when someone beat the game"""
         print(
             f"You found the word: {''.join(self.__word_to_find)} in "
@@ -118,29 +118,31 @@ class Hangman:
             f"{pluralize(self.__error_count)}!"
         )
 
-    def play_again(self) -> None:
+    def __play_again(self) -> None:
         """This is in charge of Asking the user if he wants to play again or
         not and if yes to reinitialize the game
         """
         user_answer = input("Play again y/n: ").lower()
         if user_answer == "y":
-            self.initialize_game()
+            self.__initialize_game()
             self.start_game()
         elif user_answer != "n":
-            self.play_again()
+            self.__play_again()
 
     def start_game(self) -> None:
         """Function in charge of the game workflow"""
-        print(self.__correctly_guessed_letters)
         while self.__lives > 0:
             print(f"turn {self.__turn_count}")
             # this draws the Hangman
             print(HANGMAN_PICS[self.__error_count])
-            self.play()
-            # adding an empty line for more readability
             print("")
-            print(self.__correctly_guessed_letters)
-            print(f"Wrong guesses: {self.__wrongly_guessed_letters}")
+            print(*self.__correctly_guessed_letters)
+            print("")
+            self.__play()
+            # adding empty lines for more readability
+            print(
+                f"Wrong guesses: {', '.join(self.__wrongly_guessed_letters)}"
+            )
             print(f"{self.__lives} live" f"{pluralize(self.__lives)} left")
             print(
                 f"{self.__error_count} error"
@@ -152,8 +154,8 @@ class Hangman:
 
         if self.__lives == 0:
             print(HANGMAN_PICS[self.__error_count])
-            self.game_over()
+            self.__game_over()
         else:
-            self.well_played()
+            self.__well_played()
 
-        self.play_again()
+        self.__play_again()
